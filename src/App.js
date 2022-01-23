478747b33ba212628811794e8bbc03a3d77a208d
Moath Alnajjar <moath2sweet@hotmail.com>
2022-01-23 11:09:12 +0200

    squre flexbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App app">
+      {/* <div className="container">
+        <div className="dist-one">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="dist-two">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="dist-three">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="dist-four">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+      </div> */}
+
+      <div className="container">
+        <div className="dist-one">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="dist-two">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+      </div>
+      <div className="container1">
+        <div className="dist-three">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+
+        </div>
+        <div className="dist-four">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+      </div>
     </div>
   );
 }
